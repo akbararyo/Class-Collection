eb8025d554dda2d423405819006b8ac70c0d9aec
akbararyo <akbararyo@gmail.com>
2019-06-25 12:12:40 +0700

    Add files via upload

---
+++ List(array)/Latihan2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Latihan2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat object mhs1
+            Mahasiswa mhs1 = new Mahasiswa();
+            mhs1.Nim = "2354";
+            mhs1.NamaMahasiswa = "chandra agung rizky";
+
+            //membuat object mhs2
+            Mahasiswa mhs2 = new Mahasiswa();
+            mhs2.Nim = "2314";
+            mhs2.NamaMahasiswa = "akbar aryo wicaksono";
+
+            //mebuat object dosen
+            Dosen dosen = new Dosen();
+            dosen.Nik = "0043";
+            dosen.NamaDosen = "UwU";
+
+            //membuat object list
+            List<Mahasiswa> list = new List<Mahasiswa>();
+
+            //menambahkan element list
+            //tidak ada proses boxing
+            list.Add(mhs1);
+            list.Add(mhs2);
+
+            //mengaskses element list
+            //tidak ada proses unboxing
+            Mahasiswa mhs = list[1];
+            Console.WriteLine("{0}, {1}", mhs.Nim, mhs.NamaMahasiswa);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("========");
+            Console.WriteLine(" Nim ,      nama      ");
+
+
+            foreach (Mahasiswa ab in list)
+            {
+                Console.WriteLine("{0},{1}", ab.Nim, ab.NamaMahasiswa);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ List(array)/List(array)/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_array_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat objek list khusus untuk menampung tipe data string
+            List<string> lstNamaMhs = new List<string>();
+
+            //menambahkan elemen list
+            //tidak ada proses boxing
+            lstNamaMhs.Add("erick");
+            lstNamaMhs.Add("wely");
+            lstNamaMhs.Add("rully");
+
+            //mengakses elemen list
+            //tidak ada proses unboxing
+            string namaMhs = lstNamaMhs[1];
+
+            Console.WriteLine(namaMhs);
+            Console.WriteLine();
+
+            foreach(string nama in lstNamaMhs)
+            {
+                Console.WriteLine(nama);   
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
